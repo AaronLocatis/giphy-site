145352820be0275af756ad43798fb1023a224b2a
AaronLocatis <aalocatis2@gmail.com>
2022-07-20 12:00:42 -0500

    finish context

---
+++ src/context/FavoritesContext.jsx
@@ -0,0 +1,34 @@
+import React, { useState, createContext } from "react";
+
+export const FavoritesContext = React.createContext(null);
+
+export function FavoritesProvider(props) {
+    const [favorites, setFavorites] = useState([]);
+
+    const add = useCallback(
+        (gif) => {
+            setFavorites((curr) => [...curr, gif]);
+        },
+        [setFavorites]
+    );
+
+    const remove = useCallback(
+        (id) => {
+            setFavorites((curr) => curr.filter((val) => val.gif_id !== id));
+        },
+        [setFavorites]);
+
+    const clear = useCallback(() => {
+        setFavorites([]);
+
+    }, [setFavorites]);
+
+
+
+    return (
+        <FavoritesContext.Provider value={(favorites, add, remove, clear)}>
+            (props.children)
+        </FavoritesContext.Provider>
+    );
+
+}
---
+++ src/context/UserContext.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const UserContext = React.createContext(null);
+
+export function UserProvider(props) {
+    const [loggedInUser, setLoggedInUser] = useState(null);
+    const login = useCallback(
+        (username) => {
+            setLoggedInUser(username);
+        },
+        [setLoggedInUser]
+    );
+    const logout = useCallback(() => {
+        setLoggedInUser(null);
+    }, [setLoggedInUser]);
+
+    return (
+        <UserContext.Provider value={(loggedInUser, login, logout)}>
+            (props.children)
+        </UserContext.Provider>
+    );
+}
